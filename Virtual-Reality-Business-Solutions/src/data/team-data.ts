85e21b0d9e35907402c0be8806985eaa070e2502
Mirdad1224 <ali.mirdad@yahoo.com>
2023-06-12 18:28:39 +0330

    add some data for Team component

---
+++ Virtual-Reality-Business-Solutions/src/data/team-data.ts
@@ -0,0 +1,31 @@
+import team1 from "../assets/images/team1.png";
+import team2 from "../assets/images/team2.png";
+import team3 from "../assets/images/team3.png";
+import team4 from "../assets/images/team4.png";
+
+export const teamData = [
+  {
+    id: 1,
+    img: team1,
+    name: "Sunny Khan",
+    position: "Executive officer",
+  },
+  {
+    id: 2,
+    img: team2,
+    name: "Alina Jesia",
+    position: "UX/UI DESIGNER",
+  },
+  {
+    id: 3,
+    img: team3,
+    name: "Alex Sohag",
+    position: "BUSINESS DEVELOPMENT",
+  },
+  {
+    id: 4,
+    img: team4,
+    name: "Afroza Mou",
+    position: "Head of marketing",
+  },
+];
