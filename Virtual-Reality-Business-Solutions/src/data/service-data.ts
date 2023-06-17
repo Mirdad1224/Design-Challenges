ba169b040ad523dbae61b3856f35a55ea74fa776
Mirdad1224 <ali.mirdad@yahoo.com>
2023-06-17 17:46:18 +0330

    add some data for service component

---
+++ Virtual-Reality-Business-Solutions/src/data/service-data.ts
@@ -0,0 +1,51 @@
+import service1 from "../assets/images/service1.png";
+import service2 from "../assets/images/service2.png";
+import service3 from "../assets/images/service3.png";
+import service4 from "../assets/images/service4.png";
+import service5 from "../assets/images/service5.png";
+import service6 from "../assets/images/service6.png";
+
+export const serviceData = [
+  {
+    id: 1,
+    title: "Order Management",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur the adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.",
+    icon: service1,
+  },
+  {
+    id: 2,
+    title: "Social Assistant",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur the adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.",
+    icon: service2,
+  },
+  {
+    id: 3,
+    title: "Crypto Platform",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur the adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.",
+    icon: service3,
+  },
+  {
+    id: 4,
+    title: "Smart Trading Modules",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur the adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.",
+    icon: service4,
+  },
+  {
+    id: 5,
+    title: "Analyzer of the News",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur the adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.",
+    icon: service5,
+  },
+  {
+    id: 6,
+    title: "Module of Price Notification",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur the adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.",
+    icon: service6,
+  },
+];
