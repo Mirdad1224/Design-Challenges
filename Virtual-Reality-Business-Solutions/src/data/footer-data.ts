2a99b504719b15e9f1d7e05f5b0c7f632aa6f4cc
Mirdad1224 <ali.mirdad@yahoo.com>
2023-06-17 17:49:00 +0330

    add some data for Footer component

---
+++ Virtual-Reality-Business-Solutions/src/data/footer-data.ts
@@ -0,0 +1,12 @@
+export const footerData = [
+  [
+    "Service",
+    "Order Management",
+    "Social Assistant",
+    "Crypto Platform",
+    "Analyzer of the News",
+  ],
+  ["Company", "About Us", "News", "Our trusted partner", "New users FAQ"],
+  ["Supports", "Help center", "Feedbcak", "Contact us", "Terms conditins"],
+  ["Resources", "Download app", "Blog", "What’s new", "Sitemap"],
+];
