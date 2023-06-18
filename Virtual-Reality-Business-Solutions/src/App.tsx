70d9a2e585f7e5ade7681df9f1b28415b40fe298
Mirdad1224 <ali.mirdad@yahoo.com>
2023-06-18 12:58:57 +0330

    refine App component and add the other components to it

--- Virtual-Reality-Business-Solutions/src/App.tsx
+++ Virtual-Reality-Business-Solutions/src/App.tsx
@@ -1,12 +1,24 @@
-import './App.css'
+import "./App.css";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
+import Newsletter from "./components/newsletter/Newsletter";
+import Service from "./components/service/Service";
+import Team from "./components/team/Team";
+import VRData from "./components/vr/VR";
 
 function App() {
-
   return (
-    <main className='container'>
-      hello
+    <main className="container">
+      <Navbar />
+      <Header />
+      <Service />
+      <VRData />
+      <Team />
+      <Newsletter />
+      <Footer />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
