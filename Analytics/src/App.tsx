4df91e94424cca77e73601099373207d9dab90d7
Mirdad1224 <ali.mirdad@yahoo.com>
2023-06-19 21:39:27 +0330

    refine App component

--- Analytics/src/App.tsx
+++ Analytics/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Backdrop from "./components/Backdrop";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Features from "./components/Features";
+import Services from "./components/Services";
+import Pricing from "./components/Pricing";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen((prev) => !prev);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={`mx-auto ${isSidebarOpen && "h-screen overflow-y-hidden"}`}>
+      <Backdrop isSidebarOpen={isSidebarOpen} onToggle={toggleSidebar} />
+      <Navbar isSidebarOpen={isSidebarOpen} onToggle={toggleSidebar} />
+      <Header />
+      <Features />
+      <Services />
+      <Pricing />
+    </div>
+  );
 }
 
-export default App
+export default App;
